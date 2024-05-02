9e8ee27e7d3d53e586b7d37ba1a4b4e056804ad2
CheGe <89837796+gchantura@users.noreply.github.com>
2024-05-02 16:52:50 +0200

    fixed products

--- src/routes/Products/ProductCards.svelte
+++ src/routes/Products/ProductCards.svelte
@@ -7,25 +7,25 @@
 
 {#each Products as Product}
 	<div
-		class="items-center rounded-lg bg-gray-50 shadow sm:flex dark:border-gray-700 dark:bg-gray-800"
+		class="items-center rounded-lg bg-gradient-to-r from-slate-700 to-slate-900 shadow sm:flex dark:border-gray-700 dark:bg-gray-800"
 	>
 		<div class="p-5">
-			<h3 class="py-5 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+			<h3 class="py-5 text-xl font-bold tracking-tight text-gray-300 dark:text-white">
 				{@html Product.name}
 			</h3>
 
-			<span class="py-5 text-gray-500 dark:text-gray-400"
+			<span class="py-5 leading-loose tracking-wider text-gray-300 dark:text-gray-400"
 				>{@html Product.position} -
-				<span class=" text-gray-500 dark:text-gray-100">{Product.difficulity}</span></span
+				<span class=" text-gray-100 dark:text-gray-100">{Product.difficulity}</span></span
 			>
-			<p class="mb-4 mt-3 font-light text-gray-500 dark:text-gray-400">
-				<span class="py-5 text-gray-500 dark:text-gray-400"
+			<p class="mb-4 mt-3 font-light text-gray-100 dark:text-gray-400">
+				<span class="py-5 text-gray-300 dark:text-gray-400"
 					>{@html Product.price} -
-					<span class=" text-gray-500 dark:text-gray-100">{Product.amount} ლარი</span></span
+					<span class=" text-gray-100 dark:text-gray-100">{Product.amount} ლარი</span></span
 				>
 			</p>
 
-			<p class="mb-4 mt-3 font-light text-gray-500 dark:text-gray-400">
+			<p class="mb-4 mt-3 font-light leading-loose tracking-wider text-gray-300 dark:text-gray-400">
 				{@html Product.description}
 			</p>
 		</div>
