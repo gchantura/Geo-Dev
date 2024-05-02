b4125d07d06cc9fde3c8aa45cd1f07722c2a325f
CheGe <89837796+gchantura@users.noreply.github.com>
2024-05-02 06:14:12 +0200

    fixed footer

--- src/Components/Footer/Footer.svelte
+++ src/Components/Footer/Footer.svelte
@@ -2,7 +2,9 @@
 	<div class="mx-auto w-full max-w-screen-xl">
 		<div class="grid grid-cols-2 gap-8 px-4 py-6 md:grid-cols-4 lg:py-8">
 			<div>
-				<h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">Company</h2>
+				<h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+					GeoDev.Ge
+				</h2>
 				<ul class="font-medium text-gray-500 dark:text-gray-400">
 					<li class="mb-4">
 						<a href="#" class=" hover:underline">About</a>
@@ -71,7 +73,7 @@
 		</div>
 		<div class="bg-gray-100 px-4 py-6 md:flex md:items-center md:justify-between dark:bg-gray-700">
 			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-300"
-				>© 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights Reserved.
+				>© 2024<a href="https://flowbite.com/">GeoDev</a>. All Rights Reserved.
 			</span>
 			<div class="mt-4 flex space-x-5 sm:justify-center md:mt-0 rtl:space-x-reverse">
 				<a href="#" class="text-gray-400 hover:text-gray-900 dark:hover:text-white">
