a1ca44344488d61b7b31551472b9e1a970c3b601
CheGe <89837796+gchantura@users.noreply.github.com>
2024-05-02 07:56:54 +0200

    added two buttons

--- src/routes/Main/+page.svelte
+++ src/routes/Main/+page.svelte
@@ -31,19 +31,49 @@
 
 						<p class="mb-6 text-neutral-100 dark:text-neutral-300"></p>
 
-						<p class="py-5 text-neutral-100 dark:text-neutral-300">
+						<p class="py-3 text-neutral-100 dark:text-neutral-300">
 							დიგიტალიზაციის წყალობით თქვენი ბიზნესი ხდება მარტივად ხელმისაწვდომი მომხმარებლისთვის.
 						</p>
 
-						<p class="py-5 text-neutral-100 dark:text-neutral-300">
+						<p class="py-3 text-neutral-100 dark:text-neutral-300">
 							ხელმისაწვდომობა ზრდის მომხმარებლების რაოდენობას.
 						</p>
-						<p class="py-5 text-neutral-100 dark:text-neutral-300">
+						<p class="py-3 text-neutral-100 dark:text-neutral-300">
 							მომხმარებლების რაოდენობა ზრდის თქვენს ბიზნეს.
 						</p>
-						<p class="py-5 text-neutral-100 dark:text-neutral-300">
+						<p class="py-3 text-neutral-100 dark:text-neutral-300">
 							ბიზნესის ზრდა კი თქვენს შემოსავლებს
 						</p>
+
+						<div class="mt-6 flex items-center justify-between">
+							<a
+								href="https://www.geodev.ge/order"
+								class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+							>
+								დაიწყე შეკვეთა
+								<svg
+									class="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 14 10"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M1 5h12m0 0L9 1m4 4L9 9"
+									/>
+								</svg>
+							</a>
+							<a
+								href="#WhyWe"
+								class="inline-flex items-center justify-center rounded-lg border border-white px-8 py-3 text-center text-base font-medium text-white hover:bg-gray-100 hover:text-gray-900 focus:ring-4 focus:ring-gray-400 sm:ms-4"
+							>
+								გაიგე მეტი
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -51,7 +81,7 @@
 	</section>
 </div>
 
-<div class="container mx-auto py-24 md:px-6">
+<div id="WhyWe" class="container mx-auto py-24 md:px-6">
 	<section class="mb-32">
 		<h5
 			class="mb-10 text-center text-2xl font-semibold tracking-wider md:mb-6 dark:text-neutral-300"
